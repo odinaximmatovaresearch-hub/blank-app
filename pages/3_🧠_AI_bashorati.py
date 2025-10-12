e44e7c788b089543bedd5f52eef742aec11acfb4
odinaximmatovaresearch-hub <odinaximmatova.research@gmail.com>
2025-10-12 22:47:12 +0500

    Update 3_🧠_AI_bashorati.py

--- pages/3_🧠_AI_bashorati.py
+++ pages/3_🧠_AI_bashorati.py
@@ -1,22 +1,11 @@
 import streamlit as st
-from utils.descriptors import calc_descriptors
 from models.model_loader import load_model, predict_activity
-from utils.db_manager import save_result, init_db
 
-st.header("🧠 AI faollik bashorati")
+st.title("🧠 AI bashorati")
 
-init_db()
 model = load_model()
-smiles = st.text_input("SMILES kiriting (masalan: CCO)")
+value = st.slider("Qiymatni tanlang (0 yoki 1):", 0, 1)
 
 if st.button("Bashorat qilish"):
-    desc = calc_descriptors(smiles)
-    if desc:
-        if model:
-            score = predict_activity(model, desc)
-            st.metric("Taxminiy faollik", f"{score} %")
-            save_result("demo_user", smiles, desc, score)
-        else:
-            st.warning("Model topilmadi. Demo rejimida ishlayapti.")
-    else:
-        st.error("Noto‘g‘ri SMILES.")
+    natija = predict_activity(model, value)
+    st.success(f"🔮 Model bashorati: **{natija}**")
